2358596a8a41dbaedd7dd71e4f4ad5ec23b94eb2
syedahsan297engr <syedahsannoori@gmail.com>
2023-07-13 08:56:31 +0500

    Inheritence

---
+++ Inheritence/Multiple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Mammal {
+  public:
+    Mammal() {
+      cout << "Mammals can give direct birth." << endl;
+    }
+};
+
+class WingedAnimal {
+  public:
+    WingedAnimal() {
+      cout << "Winged animal can flap." << endl;
+    }
+};
+
+class Bat: public Mammal, public WingedAnimal {};
+
+int main() {
+    Bat b1;
+    return 0;
+}
+
+//derived class inherits from 2 or more base classes --- ambiguity in func overriding
